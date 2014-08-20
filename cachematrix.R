2fd03290319ac2a20a237cabe2ac8baa53db7dcd
QinitaZ <qianzhao0303@gmail.com>
2014-08-20 18:03:55 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,11 +8,57 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        ## 0.give the value to the inverse matrix
+        m <- NULL
+        
+        ## 1.set the value of the matrix
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## 2.get the value of the matrix
+        get <- function() x
+        
+        ## 3.set the value of the inverse
+        setinverse <- function(inverse_input) m <<- inverse_input
+        
+        ## 4.get the value of the inverse
+        getinverse <- function() m
+        
+        ##reture the list of the functions above
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix" created with the above function.
+## 1. It first checks to see if the matrix has already been calculated
+## 2. If so, it gets the inverse from the cache and skips the computation;
+##    Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        ##  1. It first checks to see if the matrix has already been calculated
+        m <- x$getinverse()
+        
+        ##2. If so, it gets the inverse from the cache and skips the computation;
+        if(!is.null(m)) {
+                message("getting cached inverse")
+                return(m)
+        }
+        
+        ##   Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinverse function.
+
+        data <- x$get()
+        
+        m <- solve(data, ...)
+        
+        x$setinverse(m)
+        
+        ## and there is the final result
+        m
 }
